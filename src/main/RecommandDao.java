0519c3df637f27a2efdec175405c49371d5d7981
parkjungwhan <k2p2047@naver.com>
2015-12-17 11:06:25 +0900

    Revert "web_work"
    This reverts commit 7c2eb91d68653cfdc01018d070de38b239884529.

--- src/main/RecommandDao.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import java.util.HashMap;
-import java.util.List;
-
-public interface RecommandDao {
-	public int findbyID(int board_no, String userId)  throws Exception;
-	//public int add(RecommandVO vo) throws Exception;
-}
